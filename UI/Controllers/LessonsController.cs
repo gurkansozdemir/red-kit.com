8ac02661242f08f9f20917d20196e837f6865c1a
Pelsin <pelsin@robotikbilim.com.tr>
2019-11-14 14:54:06 +0300

    first commit

---
+++ UI/Controllers/LessonsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class LessonsController : Controller
+    {
+        // GET: Lessons
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Lesson1()
+        {
+            return View();
+        }
+        public ActionResult Lesson2()
+        {
+            return View();
+        }
+        public ActionResult Lesson3()
+        {
+            return View();
+        }
+        public ActionResult Lesson4()
+        {
+            return View();
+        }
+        public ActionResult Lesson5()
+        {
+            return View();
+        }
+        public ActionResult Lesson6()
+        {
+            return View();
+        }
+        public ActionResult Lesson7()
+        {
+            return View();
+        }
+        public ActionResult Lesson8()
+        {
+            return View();
+        }
+        public ActionResult Lesson9()
+        {
+            return View();
+        }
+        public ActionResult Lesson10()
+        {
+            return View();
+        }
+        public ActionResult Lesson11()
+        {
+            return View();
+        }
+        public ActionResult Lesson12()
+        {
+            return View();
+        }
+    }
+}
