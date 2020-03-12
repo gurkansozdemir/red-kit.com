ac63cf899575e3730d7820f953b221f9cb1066f9
MUSTAFA avcı <m.avci1974@hotmail.com>
2020-03-12 17:06:31 +0300

    update

---
+++ UI/Controllers/SingUpController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class SingUpController : Controller
+    {
+        //AtelierPortalContext db = new AtelierPortalContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //{
+        //    (doldur) currentUser = db.(doldur).Where(x => x.EMail == Email && x.Password == Password).SingleOrDefault();
+        //    if (currentUser != null)
+        //    {
+        //        Session["CurrentUser"] = currentUser;
+        //        return RedirectToAction("Index", "Home");
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Message = "Kullanıcı adı veya şifre hatalı.. Lutfen tekrar deneyiniz.";
+        //        return View();
+        //    }
+        
+        //public ActionResult Logout()
+        //{
+        //    Session["currentUser"] = null;
+        //    Session.Abandon();
+        //    return RedirectToAction("Index", "Login");
+        //}
+    }
+}
---
+++ UI/Controllers/TestsController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class TestsController : Controller
+    {
+        // GET: Tests
+        public ActionResult LedOne()
+        {
+            return View();
+        }
+        public ActionResult LedTwo()
+        {
+            return View();
+        }
+        public ActionResult LedThree()
+        {
+            return View();
+        }
+        public ActionResult LedFour()
+        {
+            return View();
+        }
+        public ActionResult ButonOne()
+        {
+            return View();
+        }
+        public ActionResult ButonTwo()
+        {
+            return View();
+        }
+        public ActionResult ButonThree()
+        {
+            return View();
+        }
+        public ActionResult ButonFour()
+        {
+            return View();
+        }
+        public ActionResult ButonFive()
+        {
+            return View();
+        }
+        public ActionResult ButonSix()
+        {
+            return View();
+        }
+    }
+}
