074a1577c78ea31895d5bbb8f12a78da4fed5436
Pelsin <pelsin@robotikbilim.com.tr>
2020-04-18 01:43:09 +0300

    asd

---
+++ UI/Controllers/MBlockController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class MBlockController : Controller
+    {
+        // GET: MBlock
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Led()
+        {
+            return View();
+        }
+        public ActionResult Buton()
+        {
+            return View();
+        }
+        public ActionResult Switch()
+        {
+            return View();
+        }
+        public ActionResult Pot()
+        {
+            return View();
+        }
+        public ActionResult Proje1()
+        {
+            return View();
+        }
+        public ActionResult RGB()
+        {
+            return View();
+        }
+        public ActionResult Buzzer()
+        {
+            return View();
+        }
+        public ActionResult MesafeSensoru()
+        {
+            return View();
+        }
+        public ActionResult Proje2()
+        {
+            return View();
+        }
+        public ActionResult LCD()
+        {
+            return View();
+        }
+        public ActionResult DHT11()
+        {
+            return View();
+        }
+        public ActionResult LDR()
+        {
+            return View();
+        }
+        public ActionResult Proje3()
+        {
+            return View();
+        }
+        public ActionResult ServoMotor()
+        {
+            return View();
+        }
+        public ActionResult PIR()
+        {
+            return View();
+        }
+        public ActionResult RFID()
+        {
+            return View();
+        }
+        public ActionResult Proje4()
+        {
+            return View();
+        }
+        public ActionResult IR()
+        {
+            return View();
+        }
+        public ActionResult Proje5()
+        {
+            return View();
+        }
+        public ActionResult IR2()
+        {
+            return View();
+        }
+    }
+}
---
+++ UI/Controllers/RedkitCController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class RedkitCController : Controller
+    {
+        // GET: RedkitC
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Led()
+        {
+            return View();
+        }
+        public ActionResult Buton()
+        {
+            return View();
+        }
+        public ActionResult Switch()
+        {
+            return View();
+        }
+        public ActionResult Potansiyometre()
+        {
+            return View();
+        }
+        public ActionResult Mod()
+        {
+            return View();
+        }
+        public ActionResult Proje1()
+        {
+            return View();
+        }
+        public ActionResult RGB()
+        {
+            return View();
+        }
+        public ActionResult Function()
+        {
+            return View();
+        }
+        public ActionResult Buzzer()
+        {
+            return View();
+        }
+        public ActionResult MesafeSensoru()
+        {
+            return View();
+        }
+        public ActionResult For()
+        {
+            return View();
+        }
+        public ActionResult Proje2()
+        {
+            return View();
+        }
+        public ActionResult LCD1()
+        {
+            return View();
+        }
+        public ActionResult LCD2()
+        {
+            return View();
+        }
+        public ActionResult Proje3()
+        {
+            return View();
+        }
+        public ActionResult DHT11()
+        {
+            return View();
+        }
+        public ActionResult LDR()
+        {
+            return View();
+        }
+        public ActionResult Proje4()
+        {
+            return View();
+        }
+        public ActionResult ServoMotor()
+        {
+            return View();
+        }
+        public ActionResult PIR()
+        {
+            return View();
+        }
+        public ActionResult RFID()
+        {
+            return View();
+        }
+        public ActionResult RFID2()
+        {
+            return View();
+        }
+        public ActionResult Proje5()
+        {
+            return View();
+        }
+        public ActionResult IR()
+        {
+            return View();
+        }
+        public ActionResult Proje6()
+        {
+            return View();
+        }
+        public ActionResult Proje7()
+        {
+            return View();
+        }
+        public ActionResult Proje8()
+        {
+            return View();
+        }
+        public ActionResult Proje9()
+        {
+            return View();
+        }
+    }
+}
